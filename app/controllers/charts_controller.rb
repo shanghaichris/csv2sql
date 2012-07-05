b419215dfe000d1f1b7f839aebd77b42d6a4c0d8
shanghaichris <shanghaichris@gmail.com>
2012-07-05 23:19:34 +0800

    add a chart to page

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,6 +2,37 @@ class ChartsController < ApplicationController
   
   
   def index
-    @question = Question.find(params[:question_id]) if params[:question_id]
+    if params[:question_id]
+      id = params[:question_id]
+      @question = Question.find(id)
+      @answers = get_answers(@question)
+     
+    end
+  end
+  
+  
+  def  get_answers(question)
+    answers = Hash.new
+    case question.id
+    when 1..6 
+      question.options.each do |option|
+        answers[option.option_id] = T_1.where("o_#{option.option_id} = ?",true).count
+      end
+        
+    when 7..17 
+      question.options.each do |option|
+        answers[option.option_id] = T_2.where("o_#{option.option_id} = ?",true).count
+      end
+    when 18..27 
+      question.options.each do |option|
+        answers[option.option_id] = T_3.where("o_#{option.option_id} = ?",true).count
+      end
+    when 28..36 
+      question.options.each do |option|
+        answers[option.option_id] = T_4.where("o_#{option.option_id} = ?",true).count
+      end
+    end
+    
+    return answers
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
