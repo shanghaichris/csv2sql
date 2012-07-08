fa89ddbac28085c9df2d0bff128098a0690e954e
shanghaichris <shanghaichris@gmail.com>
2012-07-08 10:55:11 +0800

    dailybuild

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -13,10 +13,30 @@ class ChartsController < ApplicationController
   def life
     @questions = Question.where(id: [7,9,10,11,12,13,14,15,16,17])
     @series = Question.where(id: [1,2,3,4,5,6,8])
-    if params[:question_id]
+    
+    
+    if params[:series_question]
+      @has_series = true
+    else
+      @has_series = false
+    end
+    
+    unless @has_series
+      #show total
       id = params[:question_id]
       @question = Question.find(id)
       @answers = get_answers(@question)
+    else
+      #show series
+      @question = Question.find(params[:original_question])
+      @s_options = params[:option]
+      
+      @s = Hash.new
+      debugger
+      @s_options.each do |s_option|
+        @s["#{s_option.first}_name"] = Option.find_by_option_id(s_option.first).option  # for series name
+        @s["#{s_option.first}_data"] = get_answers(@question,s_option.first)  #get etc. "0302" option as series
+      end
     end
   end
   
@@ -26,29 +46,33 @@ class ChartsController < ApplicationController
   end
   
   
-  def add_series
-    
-  end
-  
-  def  get_answers(question)
+  def  get_answers(question,s_option=nil)
     answers = Hash.new
     case question.id
     when 1..6 
       question.options.each do |option|
-        answers[option.option_id] = T_1.where("o_#{option.option_id} = ?",true).count
+
+          answers[option.option_id] = T_1.where("o_#{option.option_id} = ?",true).count
+       
       end
         
     when 7..17 
       question.options.each do |option|
-        answers[option.option_id] = T_2.where("o_#{option.option_id} = ?",true).count
+        
+          answers[option.option_id] = T_2.where("o_#{option.option_id} = ?",true).count
+        
       end
     when 18..26 
       question.options.each do |option|
-        answers[option.option_id] = T_3.where("o_#{option.option_id} = ?",true).count
+        
+          answers[option.option_id] = T_3.where("o_#{option.option_id} = ?",true).count
+        
       end
     when 27..36 
       question.options.each do |option|
-        answers[option.option_id] = T_4.where("o_#{option.option_id} = ?",true).count
+        
+          answers[option.option_id] = T_4.where("o_#{option.option_id} = ?",true).count
+       
       end
     end
     
