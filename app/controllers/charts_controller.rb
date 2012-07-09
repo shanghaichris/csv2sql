acc090c90cd828e83f6db42f95a41bac564baeea
chris <shanghaichris@gmail.com>
2012-07-09 17:25:49 +0800

    dailybuild

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -17,31 +17,37 @@ class ChartsController < ApplicationController
     
     if params[:series_question]
       @has_series = true
-    else
+      
+       #show series
+        @question = Question.find(params[:original_question])
+        @s_options = params[:option]
+
+        @s = Hash.new
+        @chart_option = ""
+        
+        debugger
+        @s_options.order("id").each do |s_option|
+          @s["#{s_option.first}_name"] = Option.find_by_option_id(s_option.first).option  # for series name
+          @s["#{s_option.first}_data"] = get_answers(@question,s_option.first)  #get etc. "0302" option as series
+          
+          @chart_option += "{name:'#{Option.find_by_option_id(s_option.first).option}',data:[]},"
+        end
+        
+        #@s:{"0302_name"=>"2 个", "0302_data"=>{"1001"=>395, ..."1002"=>581}, "0304_name"=>"4个", "0304_data"=>{"1001"=>395, ..."1002"=>581}}
+        
+        
+        
+      
+    elsif params[:question_id]
       @has_series = false
-    end
-    
-    unless @has_series
       #show total
       id = params[:question_id]
       @question = Question.find(id)
       @answers = get_answers(@question)
     else
-      #show series
-      @question = Question.find(params[:original_question])
-      @s_options = params[:option]
-      
-      @s = Hash.new
-      debugger
-      @s_options.each do |s_option|
-        @s["#{s_option.first}_name"] = Option.find_by_option_id(s_option.first).option  # for series name
-        @s["#{s_option.first}_data"] = get_answers(@question,s_option.first)  #get etc. "0302" option as series
-      end
-      
-      #@s:{"0302_name"=>"2 个", "0302_data"=>{"1001"=>395, ..."1002"=>581}, "0304_name"=>"4个", "0304_data"=>{"1001"=>395, ..."1002"=>581}}
-      
-      
+      @has_series = nil
     end
+    
   end
   
   def get_options
@@ -50,36 +56,47 @@ class ChartsController < ApplicationController
   end
   
   
-  def  get_answers(question,s_option=nil)
+  def  get_answers(question,s_option_id=nil)
     answers = Hash.new
+    
+     #if has another options to add series
+      if s_option_id
+         if "01,02,03,04,05,06".include? s_option_id[0,2]
+           s_option = T_1.find_by_o_id(s_option_id)
+         elsif "07,08,09,10,11,12,13,14,15,16,17".include? s_option_id[0,2]
+           s_option = T_2.find_by_o_id(s_option_id)
+         elsif "18,19,20,21,22,23,24,25,26,27".include? s_option_id[0,2]
+           s_option = T_3.find_by_o_id(s_option_id)
+         elsif "28,29,30,31,32,33,34,35,36".include? s_option_id[0,2]
+           s_option = T_4.find_by_o_id(s_option_id)
+         end
+
+
+      end
+    
+    
     case question.id
     when 1..6 
       question.options.each do |option|
-
           answers[option.option_id] = T_1.where("o_#{option.option_id} = ?",true).count
-       
       end
         
     when 7..17 
       question.options.each do |option|
-        
           answers[option.option_id] = T_2.where("o_#{option.option_id} = ?",true).count
-        
       end
     when 18..26 
       question.options.each do |option|
-        
           answers[option.option_id] = T_3.where("o_#{option.option_id} = ?",true).count
-        
       end
     when 27..36 
       question.options.each do |option|
-        
           answers[option.option_id] = T_4.where("o_#{option.option_id} = ?",true).count
-       
       end
     end
     
+   
+     
     return answers
   end
 end
