8db35a8b827edb07e1745953ddb50f65c128e87d
chris <shanghaichris@gmail.com>
2012-07-13 17:27:16 +0800

    dailybuild

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -61,17 +61,16 @@ class ChartsController < ApplicationController
     
      #if has another options to add series
       if s_option_id
+        table_option = ""
          if "01,02,03,04,05,06".include? s_option_id[0,2]
-           s_option = T_1.find_by_o_id(s_option_id)
+           table_option = "T_1"
          elsif "07,08,09,10,11,12,13,14,15,16,17".include? s_option_id[0,2]
-           s_option = T_2.find_by_o_id(s_option_id)
+           table_option = "T_2"
          elsif "18,19,20,21,22,23,24,25,26,27".include? s_option_id[0,2]
-           s_option = T_3.find_by_o_id(s_option_id)
+           table_option = "T_3"
          elsif "28,29,30,31,32,33,34,35,36".include? s_option_id[0,2]
-           s_option = T_4.find_by_o_id(s_option_id)
+           table_option = "T_4"
          end
-
-
       end
     
     
